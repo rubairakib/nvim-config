ccdbeb011c067a0a029c1cb20a590668a656f2ba
rubairakib <rubairakib11@gmail.com>
2025-11-13 10:22:11 +0600

    idk

--- lua/options.lua
+++ lua/options.lua
@@ -1,6 +1,58 @@
 require "nvchad.options"
-
 -- add yours here!
-
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+
+-- Force enable diagnostics and virtual text
+vim.diagnostic.enable()
+
+vim.diagnostic.config({
+  virtual_text = {
+    spacing = 4,
+    prefix = "■",  -- or use "●", "▎", "█", ""
+    -- Format function to add colors and styling
+    format = function(diagnostic)
+      if diagnostic.severity == vim.diagnostic.severity.ERROR then
+        return string.format(" %s", diagnostic.message)
+      elseif diagnostic.severity == vim.diagnostic.severity.WARN then
+        return string.format(" %s", diagnostic.message)
+      elseif diagnostic.severity == vim.diagnostic.severity.INFO then
+        return string.format(" %s", diagnostic.message)
+      elseif diagnostic.severity == vim.diagnostic.severity.HINT then
+        return string.format(" %s", diagnostic.message)
+      end
+      return diagnostic.message
+    end,
+  },
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = true,
+  float = {
+    border = "rounded",
+    source = "always",
+    header = "",
+    prefix = "",
+    format = function(diagnostic)
+      return string.format("%s: %s", diagnostic.source, diagnostic.message)
+    end,
+  },
+})
+
+-- Define custom diagnostic colors for Gruvbox
+vim.cmd([[
+  highlight DiagnosticVirtualTextError guifg=#fb4934 guibg=#3c3836 gui=italic
+  highlight DiagnosticVirtualTextWarn guifg=#fabd2f guibg=#3c3836 gui=italic
+  highlight DiagnosticVirtualTextInfo guifg=#83a598 guibg=#3c3836 gui=italic
+  highlight DiagnosticVirtualTextHint guifg=#8ec07c guibg=#3c3836 gui=italic
+  
+  highlight DiagnosticError guifg=#fb4934
+  highlight DiagnosticWarn guifg=#fabd2f
+  highlight DiagnosticInfo guifg=#83a598
+  highlight DiagnosticHint guifg=#8ec07c
+  
+  highlight DiagnosticUnderlineError gui=underline guisp=#fb4934
+  highlight DiagnosticUnderlineWarn gui=underline guisp=#fabd2f
+  highlight DiagnosticUnderlineInfo gui=underline guisp=#83a598
+  highlight DiagnosticUnderlineHint gui=underline guisp=#8ec07c
+]])
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -11,47 +11,57 @@ return {
     end,
   },
   {
-    "mrcjkb/rustaceanvim",
-    version = "^5",
-    ft = { "rust" },
-    config = function()
-      vim.g.rustaceanvim = {
-        tools = {},
-        server = {
-          on_attach = function(client, bufnr)
-            -- Enable inlay hints when LSP attaches
-            if client.server_capabilities.inlayHintProvider then
-              vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
-            end
-          end,
-          default_settings = {
-            ['rust-analyzer'] = {
-              inlayHints = {
-                bindingModeHints = { enable = true },
-                chainingHints = { enable = true },
-                closingBraceHints = { enable = true, minLines = 25 },
-                closureReturnTypeHints = { enable = "always" },
-                lifetimeElisionHints = { enable = "always", useParameterNames = true },
-                maxLength = 25,
-                parameterHints = { enable = true },
-                reborrowHints = { enable = "always" },
-                renderColons = true,
-                typeHints = { enable = true, hideNamedConstructor = false },
-              },
-            },
-          },
-        },
-        dap = {},
-      }
-    end,
-  },
-  {
     "rust-lang/rust.vim",
     ft = "rust",
     init = function()
       vim.g.rustfmt_autosave = 1
     end,
   },
+  {
+    "mrcjkb/rustaceanvim",
+      version = "^5",
+      ft = { "rust" },
+      config = function()
+        vim.g.rustaceanvim = {
+          tools = {},
+          server = {
+            on_attach = function(client, bufnr)
+              -- Disable inlay hints by default
+              if client.server_capabilities.inlayHintProvider then
+                vim.lsp.inlay_hint.enable(false, { bufnr = bufnr })
+              end
+            end,
+            default_settings = {
+              ['rust-analyzer'] = {
+                -- Enable diagnostics
+                checkOnSave = true,
+                check = {
+                  command = "clippy",
+                },
+                diagnostics = {
+                  enable = true,
+                  disabled = {},
+                  enableExperimental = true,
+                },
+                inlayHints = {
+                  bindingModeHints = { enable = true },
+                  chainingHints = { enable = true },
+                  closingBraceHints = { enable = true, minLines = 25 },
+                  closureReturnTypeHints = { enable = "always" },
+                  lifetimeElisionHints = { enable = "always", useParameterNames = true },
+                  maxLength = 25,
+                  parameterHints = { enable = true },
+                  reborrowHints = { enable = "always" },
+                  renderColons = true,
+                  typeHints = { enable = true, hideNamedConstructor = false },
+                },
+              },
+            },
+          },
+          dap = {},
+        }
+      end,
+},
   {
     'mfussenegger/nvim-dap',
     config = function()
